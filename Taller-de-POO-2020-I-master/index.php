ac5dfec171928cee104fee40c8152e98b87b6b4d
santy6221 <62039393+santy6221@users.noreply.github.com>
2020-03-11 10:46:32 -0500

    Update index.php

--- Taller-de-POO-2020-I-master/index.php
+++ Taller-de-POO-2020-I-master/index.php
@@ -14,7 +14,7 @@ require './config.php';
 $human = \entities\Managers\CharacterManager::create("Gerald", 1, 1, \entities\Races\Human::class, "Mago");
 $orc = \entities\Managers\CharacterManager::create("Garrosh", 1, 1, \entities\Races\Orc::class, "Guerrero");
 $dwarf = \entities\Managers\CharacterManager::create("Didi", 1, 1, \entities\Races\Dwarf::class, "Picaro");
-$elf = \entities\Managers\CharacterManager::create("Didi", 1, 1, \entities\Races\Elf::class, "Mago ");
+$elf = \entities\Managers\CharacterManager::create("Didielfo", 1, 1, \entities\Races\Elf::class, "Mago");
 
 \entities\Managers\DamageManager::takeDamage($human, 40, "Fisico");
 
@@ -22,11 +22,11 @@ $elf = \entities\Managers\CharacterManager::create("Didi", 1, 1, \entities\Races
 //$human->setPlayableClass("Mago");
 $golpeArma = new SkillAtk("Golpe con arma", "Se dan putazos", "Fisico", "Basico", "Atk", 1.00, 0.70, 1, 1, 1);
 $golpeTrampero = new SkillAtk("Golpe trampero", "El personaje distrae a su oponente", "Fisico", "Picaro", "Atk", 1.50, 1.50, 1, 1, 1);
-$tajoMortal=new SkillAtk("Tajo mortal", "El personaje salta con intenciones despiadas", "Fisico", "Guerrero", "Atk", 2.00, 2.00, 1, 1, 1);
-$meditacion = new SkillStats("Meditaación", "El personaje medita", "Magico", "Basico", "Stats", 1.0, 1.0, 1.05, 1.05, 1.00, 1.00);
-                                                                            //"hp"=>0,"str"=>0,"intl"=>0,"agi"=>0,"pDef"=>0,"mDef"=>0
-$calcinacion = new SkillAtk("Calcinación", "El personaje invoca el poder arcano y el elemento del fuego", "Magico", "Mago", "Atk", 1, 1, 1, 0, 40, 1);
-$tacticasCombate = new SkillStats("Tacticas de combate", "El personaje repasa el campo de batalla", "Fisico", "Avanzado", "Stats", 1, 1.05, 1, 1.05, 1, 1);
+$tajoMortal = new SkillAtk("Tajo mortal", "El personaje salta con intenciones despiadas", "Fisico", "Guerrero", "Atk", 2.00, 2.00, 1, 1, 1);
+$meditacion = new SkillStats("Meditaación", "El personaje medita", "Magico", "Basico", "Stats", 0, 0, 1.05, 1.05, 0, 0);
+//"hp"=>0,"str"=>0,"intl"=>0,"agi"=>0,"pDef"=>0,"mDef"=>0
+$calcinacion = new SkillAtk("Calcinación", "El personaje invoca el poder arcano y el elemento del fuego", "Magico", "Mago", "Atk", 0, 0, 0, 0.40, 0);
+$tacticasCombate = new SkillStats("Tacticas de combate", "El personaje repasa el campo de batalla", "Fisico", "Avanzado", "Stats", 0, 1.05, 0, 1.05, 0, 0);
 
 
 $espada1 = new Weapon("Espada de una mano", "", 1, 30, 0);
@@ -34,10 +34,10 @@ $espada2 = new Weapon("Espada de dos mano", "", 2, 30, 0);
 $hacha1 = new Weapon("Hacha de una mano", "", 2, 30, 0);
 $nada = new Weapon("", "", 1, 0, 0);
 
-$human->setWeapons([0=>$nada,1=>$nada,]);
-$orc->setWeapons([0=>$nada,1=>$nada,]);
-$dwarf->setWeapons([0=>$nada,1=>$nada,]);
-$elf->setWeapons([0=>$nada,1=>$nada,]);
+$human->setWeapons([0 => $nada, 1 => $nada,]);
+$orc->setWeapons([0 => $nada, 1 => $nada,]);
+$dwarf->setWeapons([0 => $nada, 1 => $nada,]);
+$elf->setWeapons([0 => $nada, 1 => $nada,]);
 
 \entities\Managers\WeaponManager::equipWeapon($espada1, $human, 1);
 \entities\Managers\WeaponManager::equipWeapon($espada2, $human, 1);
@@ -59,9 +59,9 @@ $elf->setWeapons([0=>$nada,1=>$nada,]);
 
 
 \entities\Managers\WeaponManager::removeWeapon($espada2, $human, 1);
-echo($human->getHands() . '<br>');
+echo ($human->getHands() . '<br>');
 \entities\Managers\WeaponManager::equipWeapon($espada2, $human, 1);
-echo($human->getHands() . '<br>');
+echo ($human->getHands() . '<br>');
 
 //$tmpSkills=$human->getSkills();
 
@@ -71,3 +71,10 @@ echo($human->getHands() . '<br>');
 \entities\Managers\DamageManager::useSkill($human, $golpeArma, $elf);
 \entities\Managers\DamageManager::useSkill($human, $golpeArma, $elf);
 \entities\Managers\DamageManager::useSkill($human, $golpeArma, $elf);
+
+\entities\Managers\DamageManager::revive($elf);
+
+
+\entities\Managers\DamageManager::useSkill($elf, $calcinacion, $human);
+
+echo $elf->getIntl();
