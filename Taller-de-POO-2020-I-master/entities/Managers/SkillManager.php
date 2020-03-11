00dd3221670b3c426beb2697df133a1c72151e3c
santy6221 <62039393+santy6221@users.noreply.github.com>
2020-03-11 11:36:51 -0500

    Update SkillManager.php

--- Taller-de-POO-2020-I-master/entities/Managers/SkillManager.php
+++ Taller-de-POO-2020-I-master/entities/Managers/SkillManager.php
@@ -7,76 +7,81 @@ namespace entities\Managers;
 use entities\Character;
 use entities\Skills\Skill;
 
-class SkillManager {
-    public static function learnSkill (Skill $skill, Character $character) {
-        $tmpSkills=$character->getSkills();
+class SkillManager
+{
 
-        if(count($tmpSkills)<$character->getMaxSkills()){
-            if(array_search($skill,$character->getSkills())==FALSE){
-                if(SkillManager::validateSkill($skill,$character)){
-                    array_push($tmpSkills,$skill);
+    //verifica si un personaje puede aprender cierta skill, y en el caso de poder, la aprende
+    public static function learnSkill(Skill $skill, Character $character)
+    {
+        $tmpSkills = $character->getSkills();
+        echo $character->getName() . " intenta aprender " . $skill->getName() . "</br>";
+
+        if (count($tmpSkills) < $character->getMaxSkills()) {
+            if (array_search($skill, $character->getSkills()) == FALSE) {
+                if (SkillManager::validateSkill($skill, $character)) {
+                    array_push($tmpSkills, $skill);
                     $character->setSkills($tmpSkills);
-                    echo ($character->getName()." ha aprendido ".$skill->getName().'<br>');
-                }else{
-                    echo ($character->getName()." no puede aprender esa skill".'<br>');
+                    echo ($character->getName() . " ha aprendido " . $skill->getName() . '<br>');
+                } else {
+                    echo ($character->getName() . " no puede aprender esa skill" . '<br>');
                 }
-            }else
-            echo ($character->getName()." ya ha aprendido ".$skill->getName()." en el pasado".'<br>');
-            
-        }else{
-            echo ($character->getName()." no puede aprender mas skills".'<br>');
+            } else
+                echo ($character->getName() . " ya ha aprendido " . $skill->getName() . " en el pasado" . '<br>');
+        } else {
+            echo ($character->getName() . " no puede aprender mas skills" . '<br>');
         }
     }
 
 
-    public static function forgetSkill (Skill $skill, Character $character) {
-        $tmpSkills=$character->getSkills();
-        $pos=array_search($skill,$tmpSkills);
+    //elimina una skill del array de estas del personaje
+    public static function forgetSkill(Skill $skill, Character $character)
+    {
+        $tmpSkills = $character->getSkills();
+        $pos = array_search($skill, $tmpSkills);
         //echo $pos.'<br>';
-        if($pos<$character->getMaxSkills()){
+        if ($pos < $character->getMaxSkills()) {
             unset($tmpSkills[$pos]);
             array_values($tmpSkills);
-            echo ($character->getName()." ha olvidado ".$skill->getName().'<br>');
+            echo ($character->getName() . " ha olvidado " . $skill->getName() . '<br>');
         }
         $character->setSkills($tmpSkills);
     }
 
-
-    public static function validateSkill(Skill $skill, Character $character){
+    //valida la compatibilidad de un personaje y una skill que desea aprender
+    public static function validateSkill(Skill $skill, Character $character)
+    {
         switch ($character->getPlayableClass()) {
             case 'Mago':
 
                 //mago aprende magicas y fisicas/basicas
-                if($skill->getType()=="Magico"){
+                if ($skill->getType() == "Magico") {
                     return true;
-                }else{
-                    if ($skill->getSubType()=="Basico") {
-                    return true;
+                } else {
+                    if ($skill->getSubType() == "Basico") {
+                        return true;
                     }
                 }
                 break;
             case 'Guerrero':
 
                 //los guerreros solo aprender fisicos
-                if($skill->getType()=="Fisico"){
+                if ($skill->getType() == "Fisico") {
                     return true;
                 }
                 break;
             case 'Picaro':
 
                 //los picaros aprender basicas y propias
-                if($skill->getSubType()=="Basico"||$skill->getSubType()=="Picaro"){
+                if ($skill->getSubType() == "Basico" || $skill->getSubType() == "Picaro") {
                     return true;
                 }
                 break;
-            
+
             default:
-                echo ("Las clases admitidas son: 'Mago', 'Guerrero' y 'Picaro'".'<br>');
+                echo ("Las clases admitidas son: 'Mago', 'Guerrero' y 'Picaro'" . '<br>');
                 return false;
                 break;
         }
         return false;
     }
-
-
 }
